ca08cd6753ff4f81b27fc55dc882e2cc7167d6cf
chaves <chavesprogrammer@gmail.com>
2025-07-01 09:24:05 -0300

    inicial

---
+++ src/main/java/com/devchaves/Pork_backend/PorkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devchaves.Pork_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devchaves/Pork_backend/PorkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devchaves.Pork_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

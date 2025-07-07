2fd24c9bc328d93a80a683f4e58a9772baa1f26e
dev-Chaves <chavesprogrammer@gmail.com>
2025-07-07 20:24:24 -0300

    inicio da service para despesas

---
+++ src/main/java/com/devchaves/Pork_backend/repository/ExpenseRepository.java
@@ -0,0 +1,15 @@
+package com.devchaves.Pork_backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devchaves.Pork_backend.entity.ExpenseEntity;
+
+public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
+
+    @Query(value = "SELECT * FROM tb_despesas WHERE categoria = :categoria", nativeQuery = true)
+    public List<ExpenseEntity> findByCategory(String category);
+
+}
---
+++ src/main/java/com/devchaves/Pork_backend/services/ExpensesService.java
@@ -0,0 +1,58 @@
+package com.devchaves.Pork_backend.services;
+
+import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.devchaves.Pork_backend.entity.ExpenseEntity;
+import com.devchaves.Pork_backend.entity.UserEntity;
+import com.devchaves.Pork_backend.repository.ExpenseRepository;
+import com.devchaves.Pork_backend.repository.UserRepository;
+
+@Service
+public class ExpensesService {
+
+    private final ExpenseRepository expenseRepository;
+
+    private final UserRepository userRepository;
+
+    public ExpensesService(ExpenseRepository expenseRepository, UserRepository userRepository) {
+        this.expenseRepository = expenseRepository;
+        this.userRepository = userRepository;
+    }
+
+    public List<ExpenseEntity> cadastrarDespesas( ){
+
+       UserEntity user = getCurrentUser();
+
+       if (user.getVerificado() == false) {
+        throw new IllegalStateException("Usuário não verificado!");
+       }
+
+       ExpenseEntity despesas = new ExpenseEntity();
+
+       des
+        
+
+        return null;
+
+    }
+
+    private UserEntity getCurrentUser (){
+        try{
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+            if(authentication != null && authentication.getPrincipal() instanceof UserEntity){
+                return (UserEntity) authentication.getPrincipal();
+            }
+        }catch(Exception e){
+            throw new UsernameNotFoundException("Usuário não encontrado!");
+        }
+        
+        return null;
+    }
+
+}   

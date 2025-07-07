eb144521d2288d5e17722315e4364f0eb72937a0
chaves <chavesprogrammer@gmail.com>
2025-07-07 14:12:09 -0300

    métodos para verificar o token

--- src/main/java/com/devchaves/Pork_backend/services/TokenService.java
+++ src/main/java/com/devchaves/Pork_backend/services/TokenService.java
@@ -1,10 +1,17 @@
 package com.devchaves.Pork_backend.services;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.devchaves.Pork_backend.entity.UserEntity;
 import com.devchaves.Pork_backend.entity.VerificationTokenEntity;
 import com.devchaves.Pork_backend.repository.TokenRepository;
@@ -12,6 +19,11 @@ import com.devchaves.Pork_backend.repository.UserRepository;
 
 @Service
 public class TokenService {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    private final long expirationTime = 86400000;
 
     private final TokenRepository tokenRepository;
 
@@ -22,14 +34,14 @@ public class TokenService {
         this.userRepository = userRepository;
     }
 
-    public void verificarToken(String request){
+    public void verificarToken(String request) {
 
-        if(request == null || request.isEmpty()){
+        if (request == null || request.isEmpty()) {
             throw new IllegalArgumentException("Token não pode ser inválido ou vazio!");
         }
 
         VerificationTokenEntity token = tokenRepository.findByToken(request);
-        
+
         verificarExpiracao(token);
 
         UserEntity user = token.getUser();
@@ -46,14 +58,80 @@ public class TokenService {
 
     }
 
-
-    public static void verificarExpiracao(VerificationTokenEntity token){
+    public static void verificarExpiracao(VerificationTokenEntity token) {
 
         LocalDateTime now = LocalDateTime.now();
 
-        if(token.getExpira_em().isBefore(now)){
+        if (token.getExpira_em().isBefore(now)) {
             throw new IllegalArgumentException("Token expirado!");
         }
-        
+
+    }
+
+    public String generateToken(UserEntity user){
+
+        try{
+            if(user == null){
+                throw new IllegalArgumentException("Usuário não pode ser nulo!");
+            }
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            Date expirationDate = new Date(System.currentTimeMillis() + expirationTime);
+
+            String token = JWT.create()
+                .withSubject(user.getEmail())
+                .withIssuer("API PORK")
+                .withExpiresAt(expirationDate)
+                .sign(algorithm);
+
+                return token;
+
+        }catch(JWTCreationException e){
+            throw new RuntimeException("Error ao criar o token", e);
+        }
+    }
+
+    public String estaValido(String token){
+
+        try {
+            if(token == null){
+                throw new IllegalArgumentException("Token não pode ser nulo");
+            }
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            String subject = JWT.require(algorithm)
+            .withIssuer("API PORK")
+            .build()
+            .verify(token)
+            .getSubject();
+
+            return subject;
+        }catch(JWTVerificationException e){
+            throw new JWTVerificationException("Erro a válidar token");
+        }
+    }
+
+    public Date getDateExpiration(String token) {
+
+        try {
+            if(token == null || token.isEmpty()){
+                return null;
+            }
+
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            Date expirationDate = JWT.require(algorithm)
+                    .withIssuer("API PORK")
+                    .build()
+                    .verify(token)
+                    .getExpiresAt();
+
+            return expirationDate;
+
+        } catch (Exception e){
+            throw new RuntimeException("Error getting expiration date: " + e.getMessage());
+        }
+
     }
 }

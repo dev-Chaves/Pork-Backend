d438b3835ff05d2ea055ea07c4343c47430311b1
chaves <chavesprogrammer@gmail.com>
2025-07-07 14:46:04 -0300

    reajustando para pegar a chave jwt

---
+++ src/main/java/com/devchaves/Pork_backend/DTO/LoginResponseDTO.java
@@ -0,0 +1,8 @@
+package com.devchaves.Pork_backend.DTO;
+
+public record LoginResponseDTO(
+    String token,
+    String email
+) {
+
+}
--- src/main/java/com/devchaves/Pork_backend/services/TokenService.java
+++ src/main/java/com/devchaves/Pork_backend/services/TokenService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.devchaves.Pork_backend.entity.UserEntity;
 import com.devchaves.Pork_backend.entity.VerificationTokenEntity;

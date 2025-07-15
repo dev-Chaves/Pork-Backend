3f8658860fece1b0b7108e4ef147c5701c2cf1f0
chaves <chavesprogrammer@gmail.com>
2025-07-15 11:05:25 -0300

    add endpoint para consultar gastos'

--- src/main/java/com/devchaves/Pork_backend/repository/ExpenseRepository.java
+++ src/main/java/com/devchaves/Pork_backend/repository/ExpenseRepository.java
@@ -6,14 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.devchaves.Pork_backend.entity.ExpenseEntity;
-import com.devchaves.Pork_backend.entity.UserEntity;
 
 public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
 
     @Query(value = "SELECT * FROM tb_despesas WHERE categoria = :categoria", nativeQuery = true)
     public List<ExpenseEntity> findByCategory(String category);
 
-    @Query(value = "SELECT * FROM tb_despesas WHERE usuario = :user", nativeQuery = true)
-    public List<ExpenseEntity> findByUser(UserEntity user);
+    @Query(value = "SELECT * FROM tb_despesas WHERE user_id = :userId", nativeQuery = true)
+    public List<ExpenseEntity> findByUser(Long userId);
 
 }

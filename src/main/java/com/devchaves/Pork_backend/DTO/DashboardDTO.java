8edecac1f9cebf2ced73c435bb40a4abca1858ee
dev-Chaves <chavesprogrammer@gmail.com>
2025-07-14 23:24:30 -0300

    dashboard dto

---
+++ src/main/java/com/devchaves/Pork_backend/DTO/DashboardDTO.java
@@ -0,0 +1,15 @@
+package com.devchaves.Pork_backend.DTO;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record DashboardDTO(
+
+    List<ExpenseResponseDTO> todasDespesas, 
+    List<ExpenseResponseDTO> variaveis, 
+    List<ExpenseResponseDTO> fixas, 
+    BigDecimal totalDespesas
+
+) {
+
+}

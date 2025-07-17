861ae2b89f681d7ca5f91872aff4f3ffd6792a62
dev-Chaves <chavesprogrammer@gmail.com>
2025-07-16 22:49:12 -0300

    ajustes final

--- src/main/java/com/devchaves/Pork_backend/controller/ExpenseController.java
+++ src/main/java/com/devchaves/Pork_backend/controller/ExpenseController.java
@@ -18,7 +18,7 @@ import jakarta.validation.Valid;
 
 
 @RestController
-@RequestMapping("despesas")
+@RequestMapping("/despesas")
 public class ExpenseController {
 
     private final ExpensesService expensesService;
--- src/main/java/com/devchaves/Pork_backend/services/ExpensesService.java
+++ src/main/java/com/devchaves/Pork_backend/services/ExpensesService.java
@@ -6,9 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devchaves.Pork_backend.DTO.DashboardDTO;

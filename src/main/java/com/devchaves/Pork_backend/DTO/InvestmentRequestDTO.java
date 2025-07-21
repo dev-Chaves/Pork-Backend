62204177f457602d0856e01c3236e75d80cef1d8
chaves <chavesprogrammer@gmail.com>
2025-07-21 09:06:57 -0300

    dtos para investimento

---
+++ src/main/java/com/devchaves/Pork_backend/DTO/InvestmentRequestDTO.java
@@ -0,0 +1,6 @@
+package com.devchaves.Pork_backend.DTO;
+
+import com.devchaves.Pork_backend.ENUM.InvestimentoENUM;
+
+public record InvestmentRequestDTO(InvestimentoENUM tipo) {
+}
---
+++ src/main/java/com/devchaves/Pork_backend/services/InvestmentService.java
@@ -0,0 +1,4 @@
+package com.devchaves.Pork_backend.services;
+
+public class InvestmentService {
+}

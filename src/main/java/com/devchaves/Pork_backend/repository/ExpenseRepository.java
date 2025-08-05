dc99206f3edea938b8ca2ea8dc2624561cc882dd
chaves <chavesprogrammer@gmail.com>
2025-08-05 11:39:30 -0300

    método para cadastro e busca de receita atualizado

--- src/main/java/com/devchaves/Pork_backend/repository/ExpenseRepository.java
+++ src/main/java/com/devchaves/Pork_backend/repository/ExpenseRepository.java
@@ -4,6 +4,7 @@ import com.devchaves.Pork_backend.entity.ExpenseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
@@ -16,5 +17,14 @@ public interface ExpenseRepository extends JpaRepository<ExpenseEntity, Long> {
 
     @Query(value = "SELECT * FROM tb_despesas WHERE id = :id AND user_id = :userId", nativeQuery = true)
     public ExpenseEntity findByIdAndUserId(Long id, Long userId);
+
+    @Query(value = "SELECT * FROM tb_despesas WHERE user_id = :user_id AND categoria = 'VARIAVEL'", nativeQuery = true)
+    public List<ExpenseEntity> findVariablesExpensesByUserId(Long userId);
+
+    @Query(value = "SELECT * FROM tb_despesas WHERE user_id = :user_id AND categoria = 'FIXA'", nativeQuery = true)
+    public List<ExpenseEntity> findFixedsExpensesByUserId(Long userId);
+
+    @Query(value = "SELECT SUM(valor) FROM tb_despesas WHERE user_id = :userId", nativeQuery = true)
+    public BigDecimal sumTotalExpenseByUserId(Long userId);
 
 }

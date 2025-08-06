10cb0af8da917b86cf3e1a1efaae8b704fc509a8
chaves <chavesprogrammer@gmail.com>
2025-08-06 10:56:01 -0300

    tratamento de erro genérico

--- src/main/java/com/devchaves/Pork_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devchaves/Pork_backend/exception/GlobalExceptionHandler.java
@@ -19,4 +19,10 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception ex) {
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocorreu um erro interno no servidor.");
+    }
+
 }

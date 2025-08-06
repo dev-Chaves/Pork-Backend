13633808d4650ac9ba36283fd45a757c9f8e3033
chaves <chavesprogrammer@gmail.com>
2025-08-06 15:26:15 -0300

    inicio

--- src/main/java/com/devchaves/Pork_backend/PorkBackendApplication.java
+++ src/main/java/com/devchaves/Pork_backend/PorkBackendApplication.java
@@ -2,8 +2,10 @@ package com.devchaves.Pork_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PorkBackendApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/devchaves/Pork_backend/config/RedisConfig.java
@@ -0,0 +1,17 @@
+package com.devchaves.Pork_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+
+}

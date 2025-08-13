bfcb8cdf31918c8528f8f31899de64a5bdb4ced9
chaves <chavesprogrammer@gmail.com>
2025-08-13 10:18:31 -0300

    especificando a chave do cache

--- src/main/java/com/devchaves/Pork_backend/controller/UserController.java
+++ src/main/java/com/devchaves/Pork_backend/controller/UserController.java
@@ -3,6 +3,8 @@ package com.devchaves.Pork_backend.controller;
 import com.devchaves.Pork_backend.DTO.UserInfoResponse;
 import com.devchaves.Pork_backend.services.UserService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +20,7 @@ public class UserController {
     }
 
     @GetMapping("info")
-    public ResponseEntity<UserInfoResponse> consultarInfo(){
-        return ResponseEntity.ok(userService.consultarInfo());
+    public ResponseEntity<UserInfoResponse> consultarInfo(@AuthenticationPrincipal UserDetails userDetails){
+        return ResponseEntity.ok(userService.consultarInfo(userDetails));
     }
 }

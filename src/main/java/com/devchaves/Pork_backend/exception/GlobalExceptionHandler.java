8af9fd8822e103ccbc55bc07c811658c1ff44115
chaves <chavesprogrammer@gmail.com>
2025-09-02 20:18:34 -0300

    adicionando logs no exception handler

--- src/main/java/com/devchaves/Pork_backend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/devchaves/Pork_backend/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.devchaves.Pork_backend.exception;
 
 import com.devchaves.Pork_backend.DTO.ErrorResponseDTO;
 import org.apache.tomcat.websocket.AuthenticationException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +21,11 @@ import java.util.stream.Collectors;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ErrorResponseDTO> handleIllegalArgument(IllegalArgumentException ex) {
-
+        logger.error("Parâmetros inválidos foram fornecidos.", ex);
         ErrorResponseDTO response = new ErrorResponseDTO(
             HttpStatus.BAD_REQUEST.value(),
             "Parâmetros Inválidos",
@@ -34,7 +38,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponseDTO> handleMethodArgumentNotValid(MethodArgumentNotValidException e){
-
+        logger.error("Erro de validação nos argumentos do método.", e);
         List<String> erros = e.getBindingResult().getFieldErrors()
                 .stream().map(f -> f.getField() + ": " + f.getDefaultMessage()).toList();
 
@@ -51,7 +55,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ErrorResponseDTO> handleIllegalState(IllegalStateException e){
-
+        logger.error("Estado ilegal encontrado durante o processamento.", e);
         ErrorResponseDTO response = new ErrorResponseDTO(
                 HttpStatus.CONFLICT.value(),
                 "Conflito na regra de negócio",
@@ -64,7 +68,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(UsernameNotFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleUsernameNotFound(UsernameNotFoundException ex){
-
+        logger.error("Tentativa de acesso com usuário não encontrado.", ex);
         ErrorResponseDTO response = new ErrorResponseDTO(
                 HttpStatus.BAD_REQUEST.value(),
                 "Usuário não encontrado",
@@ -76,6 +80,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDTO> handleGenericException(Exception ex) {
+        logger.error("Ocorreu uma exceção não tratada.", ex);
         ErrorResponseDTO response = new ErrorResponseDTO(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 "Erro no processo",
@@ -86,7 +91,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoHandlerFoundException.class)
     public ResponseEntity<ErrorResponseDTO> handleNotFound(NoHandlerFoundException e){
-
+        logger.warn("Nenhum manipulador encontrado para a requisição: {}", e.getRequestURL());
         String message = "Recurso não encontrado: A rota " + e.getRequestURL() + " não existe.";
 
         ErrorResponseDTO response = new ErrorResponseDTO(
@@ -100,6 +105,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AuthenticationException.class)
     public ResponseEntity<ErrorResponseDTO> handleAuthenticationException(AuthenticationException e){
+        logger.warn("Falha na autenticação.", e);
         ErrorResponseDTO response = new ErrorResponseDTO(
                 HttpStatus.UNAUTHORIZED.value(),
                 "Autênticação Falhou",
@@ -111,6 +117,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<ErrorResponseDTO> handleAccessDenied(AccessDeniedException e){
+        logger.warn("Acesso negado.", e);
         ErrorResponseDTO response = new ErrorResponseDTO(
             HttpStatus.FORBIDDEN.value(),
             "Acesso negado",
@@ -122,6 +129,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorResponseDTO> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+        logger.warn("Não foi possível ler a mensagem HTTP.", ex);
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(
                 HttpStatus.BAD_REQUEST.value(),
                 "Requisição Malformada",
@@ -132,6 +140,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ErrorResponseDTO> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        logger.error("Violação de integridade dos dados.", ex);
         ErrorResponseDTO errorResponse = new ErrorResponseDTO(
                 HttpStatus.CONFLICT.value(),
                 "Conflito de Dados",

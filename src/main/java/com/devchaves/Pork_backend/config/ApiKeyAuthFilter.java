698e6f2cbdba4734ac4a563736be8d5cd157c467
chaves <chavesprogrammer@gmail.com>
2025-09-29 12:24:44 -0300

    criando api-filter para chave vindo dos agents

--- src/main/java/com/devchaves/Pork_backend/config/ApiKeyAuthFilter.java
+++ src/main/java/com/devchaves/Pork_backend/config/ApiKeyAuthFilter.java
@@ -1,4 +1,39 @@
 package com.devchaves.Pork_backend.config;
 
-public class ApiKeyAuthFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
+    @Value("${pork.monitoring.api-key}")
+    private String secretApiKey;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String requestApiKey = request.getHeader("X-API-KEY");
+
+
+
+        if (secretApiKey.equals(requestApiKey)){
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    "AGENTE_USER",
+                    null,
+                    AuthorityUtils.createAuthorityList("ROLE_AGENT")
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
 }
